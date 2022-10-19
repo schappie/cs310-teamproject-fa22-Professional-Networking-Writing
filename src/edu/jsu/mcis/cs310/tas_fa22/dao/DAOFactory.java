b181d29e9410517494284125ea10bb5225c54cef
eparker65 <91712654+eparker65@users.noreply.github.com>
2022-10-19 12:02:05 -0500

    Wrote shift object into main class

--- src/edu/jsu/mcis/cs310/tas_fa22/dao/DAOFactory.java
+++ src/edu/jsu/mcis/cs310/tas_fa22/dao/DAOFactory.java
@@ -35,5 +35,9 @@ public final class DAOFactory {
     public BadgeDAO getBadgeDAO() {
         return new BadgeDAO(this);
     }
+    
+    public ShiftDAO getShiftDAO(){
+        return new ShiftDAO(this);
+    }
 
 }
