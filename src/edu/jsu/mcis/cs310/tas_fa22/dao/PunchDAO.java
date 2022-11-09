e040d99cb67913d902351e0ab061bb041e5461bb
Austin Sanderson <asanderson2@stu.jsu.edu>
2022-11-09 12:05:10 -0600

    Create method worked on and comments.

--- src/edu/jsu/mcis/cs310/tas_fa22/dao/PunchDAO.java
+++ src/edu/jsu/mcis/cs310/tas_fa22/dao/PunchDAO.java
@@ -2,6 +2,7 @@ package edu.jsu.mcis.cs310.tas_fa22.dao;
 
 import edu.jsu.mcis.cs310.tas_fa22.*;
 import java.sql.*;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -48,7 +49,7 @@ public class PunchDAO {
                         Badge badge = badgeDAO.find(badgeid);
                         EventType punchtype = EventType.values()[rs.getInt("eventtypeid")];
                         LocalDateTime originaltimestamp = rs.getTimestamp("timestamp").toLocalDateTime();
-                        
+  
                         punch = new Punch(id, terminalid, badge, originaltimestamp, punchtype);
                     }
 
@@ -90,8 +91,8 @@ public class PunchDAO {
     
     public Integer create(Punch p){
            
-
-
+        int punchID = 0;
+        // "INSERT INTO event (terminalid, badgeid, timestamp, eventtype) VALUES(?, ?, ?, ?)"
         PreparedStatement ps = null;
         ResultSet rs = null;
 
@@ -101,8 +102,13 @@ public class PunchDAO {
 
             if (conn.isValid(0)) {
 
-                ps = conn.prepareStatement(QUERY_CREATE);
-                //ps.setString();
+                ps = conn.prepareStatement(QUERY_CREATE, PreparedStatement.RETURN_GENERATED_KEYS);
+                // Slide 30 of 30 JavaDatabaseProgramming 
+                //ps.setString(1, terminalid);
+                //ps.setString(2, badgeid);
+                //ps.setString(3, (new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")).format(originaltimestamp.getTime()));
+                //ps.setString(4, eventtype);
+                
 
                 boolean hasresults = ps.execute();
 
@@ -111,7 +117,7 @@ public class PunchDAO {
                     rs = ps.getResultSet();
 
                     while (rs.next()) {
-                     
+                        
                     }
 
                 }
