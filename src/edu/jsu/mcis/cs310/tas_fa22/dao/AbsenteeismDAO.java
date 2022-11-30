6a7bc8b0623970a7fed93e98ce592a906b205f4e
Sawyer Chapman <schapman1@stu.jsu.edu>
2022-11-30 11:34:37 -0600

    Creating Absenteeism and DAO

---
+++ src/edu/jsu/mcis/cs310/tas_fa22/dao/AbsenteeismDAO.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.jsu.mcis.cs310.tas_fa22.dao;
+
+/**
+ *
+ * @author sawyerchapman
+ */
+public class AbsenteeismDAO {
+    
+}
