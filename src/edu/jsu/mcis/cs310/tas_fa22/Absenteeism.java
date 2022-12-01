937a0357e36d6c8d6479915ce050ff51691b4118
Your NameAntonio <Amadison1@stu.jsu.edu>
2022-12-01 16:08:49 -0600

    absenteeism module class done.

--- src/edu/jsu/mcis/cs310/tas_fa22/Absenteeism.java
+++ src/edu/jsu/mcis/cs310/tas_fa22/Absenteeism.java
@@ -4,10 +4,45 @@
  */
 package edu.jsu.mcis.cs310.tas_fa22;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 /**
  *
  * @author sawyerchapman
  */
 public class Absenteeism {
+    
+    private final Employee employee;
+    private final LocalDate paystart;
+    private final float percentage;
+
+    public Absenteeism(Employee employee, LocalDate paystart, float percentage) {
+        this.employee = employee;
+        this.paystart = paystart;
+        this.percentage = percentage;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public LocalDate getPaystart() {
+        return paystart;
+    }
+
+    public float getPercentage() {
+        return percentage;
+    }
+
+    @Override
+    public String toString() {
+        return "Absenteeism{" + "employee=" + employee + ", paystart=" + paystart + ", percentage=" + percentage + '}';
+    }
+    
+    
+
+     
+    
     
 }
